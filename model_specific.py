39230f24b4531a749101bd57b7353006ca33f10d
Wojtek Zarzecki <wojtek.zarzecki2002@gmail.com>
2024-07-26 08:14:14 +0200

    feat: add submodule for benchmarking

---
+++ model_specific.py
@@ -0,0 +1,34 @@
+""" module for model specific concretisation of abstract interfaces """
+import os
+
+import numpy as np
+import pandas as pd
+
+from tools.converter import Converter, InputConverter
+from tools.inference import Inferencer
+
+
+class ConcreteConverter(Converter):
+    def process_file(self, filepath: str, output_filename: str):
+        """ implement for specific model
+        expects tsv file with columns:
+        classifier: Prediction, Probability_score
+        regressor: Prediction"""
+        df = pd.read_csv(filepath)
+        df.rename(columns={"Prediction": "Probability_score"}, inplace=True)
+        df["Prediction"] = np.where((df["Probability_score"] >= 0.5), "AMP", "non-AMP")
+        df.to_csv(output_filename, sep="\t")
+
+
+class ConcreteInferencer(Inferencer):
+    def process_file(self, filepath: str, output_filename: str):
+        """ implement for specific model """
+        command = f"python -m amp.inference.scripts.predict_if_amp --model_path models/amp_classifier/ \
+--sequence_path {filepath} --format fasta --output_csv {output_filename}"
+        print(command)
+        os.system(command)
+
+
+class ConcreteInputConverter(InputConverter):
+    def process_file(self, filepath: str, output_filename: str):
+        pass
